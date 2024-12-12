585500b9b5f7647928bb72ecef5d9cb9cecbb9ea
tdk-bwcb <ygf2024@gmail.com>
2024-12-12 16:54:15 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,45 @@
+let score = 33
+// console.log(typeof score) // -> number
+
+let score1 = "33"
+// console.log(typeof score1) // -> string
+
+let valueInNumber = Number(score1)
+// console.log(typeof valueInNumber) // -> number
+// console.log(valueInNumber)
+
+
+let score2 = "33gsh"
+// console.log(typeof score2) // -> string
+let valueInNumber2 = Number(score2)
+// console.log(typeof valueInNumber2) // -> number
+// console.log(valueInNumber2) // -> NaN (Not a NUmber)
+
+let score3 = null
+// console.log(typeof score3) // -> object
+let valueInNumber3 = Number(score3)
+// console.log(typeof valueInNumber3) // -> number
+// console.log(valueInNumber3) // -> 0
+
+let score4 =  undefined
+// console.log(typeof score4) // -> undefined
+let valueInNumber4 = Number(score4)
+// console.log(typeof valueInNumber4) // -> number
+// console.log(valueInNumber4) // -> NaN (Not a NUmber)
+
+
+
+// "33" => 33
+// "33bas" => NaN
+// true => 1, false => 0
+
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // -> true(isLoggedIn = 1, "nonEmptyString") false(isLoggedIn = 0, "")
+
+
+let someNum = 33
+let stringNumber = String(someNum)
+console.log(stringNumber) // -> 33
+console.log(typeof stringNumber) // -> string
