1df0a0e815bad4a5c09477cef1cd265d9e0662d7
tdk-bwcb <ygf2024@gmail.com>
2024-12-12 15:04:24 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "yash@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+// accountId = 25361 // cannot change const
+accountEmail = "iugfoifisi"
+accountPassword = "ewgt234"
+accountCity = "iugfoipoifh"
+
+let acccountState; // -> undefined
+console.table([accountEmail, accountId, accountPassword, accountCity, acccountState])
+
+/*
+Prefer not to use var -> issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // traet all js code as newer version
+
+// alert("uhyi")
+// we are using nodejs, not browser
+
+let name = "Yash"
+let age = 19
+let isLoggedIn = false
+
+// number => 2^53
+// bigint
+// string => " "
+// boolean => true / false
+// null => standalone value
+// undefined => var declared, value not defined
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof null); // -> object
+console.log(typeof undefined); // -> undefined
