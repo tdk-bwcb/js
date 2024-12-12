d06681707d196033e8c1a5c532ef1a6625b03b85
tdk-bwcb <ygf2024@gmail.com>
2024-12-12 17:29:37 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2>1) // -> true
+
+// console.log("2" > 1); // -> true
+// console.log("02" > 1); // -> true
+
+// console.log(null > 0); // -> false
+// console.log(null == 0); // -> false
+// console.log(null >= 0); // -> true 
+// equailty(==) compare as it is, comparison operators convert it to numbers first
+
+// console.log(undefined > 0); // -> false
+// console.log(undefined == 0); // -> false
+// console.log(undefined < 0); // -> false
+
+// ===
+
+console.log("2" === 2) // -> false (diff data types)
+console.log("2" == 2) // -> true (conversion ✅)
